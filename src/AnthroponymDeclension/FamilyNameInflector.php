b905946ec924a230c60ff44658bb2d86acf44d6d
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-04-01 19:05:25 +0000

    chore: Add FamilyNameInflector class

---
+++ src/AnthroponymDeclension/FamilyNameInflector.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shevchenko\AnthroponymDeclension;
+
+use Shevchenko\Language\GrammaticalCase;
+use Shevchenko\Language\GrammaticalGender;
+use Shevchenko\Language\Linguistics;
+use Shevchenko\Language\WordClass;
+use Shevchenko\WordDeclension\Enums\ApplicationType;
+use Shevchenko\WordDeclension\WordInflector;
+
+class FamilyNameInflector extends NameInflector
+{
+    private const UNCERTAIN_FEMININE_PATTERN = '/[ая]$/iu';
+    private const UNCERTAIN_MASCULINE_PATTERN = '/(ой|ий|ій|их)$/iu';
+
+    public function __construct(WordInflector $wordInflector)
+    {
+        parent::__construct($wordInflector);
+    }
+
+    protected function inflectNamePart(
+        string $familyName,
+        GrammaticalGender $gender,
+        GrammaticalCase $grammaticalCase,
+        bool $isLastWord = true,
+    ): string {
+        if (!$isLastWord && Linguistics::isMonosyllable($familyName)) {
+            return $familyName;
+        }
+
+        $wordClass = $this->determineWordClass($familyName, $gender);
+
+        return $this->wordInflector->inflect(
+            $familyName,
+            $grammaticalCase,
+            $gender,
+            wordClass: $wordClass,
+            applicationType: ApplicationType::FAMILY_NAME,
+        );
+    }
+
+    private function determineWordClass(string $familyName, GrammaticalGender $gender): WordClass
+    {
+        if ($gender === GrammaticalGender::FEMININE && preg_match(self::UNCERTAIN_FEMININE_PATTERN, $familyName)) {
+            return WordClass::ADJECTIVE;
+        }
+
+        if ($gender === GrammaticalGender::MASCULINE && preg_match(self::UNCERTAIN_MASCULINE_PATTERN, $familyName)) {
+            return WordClass::ADJECTIVE;
+        }
+
+        return WordClass::NOUN;
+    }
+}
