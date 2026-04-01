a247ea7a6bd583c0a1ba4a7bb0198eefc38677fd
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-04-01 19:04:14 +0000

    chore: Add Linguistics utility class

---
+++ src/Language/Linguistics.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shevchenko\Language;
+
+final class Linguistics
+{
+    private const VOWEL_PATTERN = '/[аоуеиіяюєї]/iu';
+
+    public static function countSyllables(string $word): int
+    {
+        preg_match_all(self::VOWEL_PATTERN, $word, $matches);
+        return count($matches[0]);
+    }
+
+    public static function isMonosyllable(string $word): bool
+    {
+        return self::countSyllables($word) === 1;
+    }
+}
