afe37768e0462b07aa3b306f5c8b8203be7b9262
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-04-01 19:05:07 +0000

    chore: Add DeclensionRuleLoader class

---
+++ src/WordDeclension/DeclensionRuleLoader.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shevchenko\WordDeclension;
+
+use Shevchenko\Language\GrammaticalGender;
+use Shevchenko\Language\WordClass;
+
+class DeclensionRuleLoader
+{
+    /** @return array<DeclensionRule> */
+    public static function loadFromFile(string $filePath): array
+    {
+        $json = file_get_contents($filePath);
+        $data = json_decode($json, true, 512, JSON_THROW_ON_ERROR);
+
+        return array_map(self::parseRule(...), $data);
+    }
+
+    /** @param array<string, mixed> $data */
+    private static function parseRule(array $data): DeclensionRule
+    {
+        return new DeclensionRule(
+            description: $data['description'],
+            examples: $data['examples'],
+            wordClass: WordClass::from($data['wordClass']),
+            gender: array_map(GrammaticalGender::from(...), $data['gender']),
+            priority: $data['priority'],
+            applicationType: $data['applicationType'] ?? [],
+            pattern: new DeclensionPattern($data['pattern']['find'], $data['pattern']['modify']),
+            grammaticalCases: new GrammaticalCases($data['grammaticalCases']),
+        );
+    }
+}
