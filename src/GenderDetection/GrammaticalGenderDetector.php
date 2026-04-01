74e70c2ee149532f91cf85b643a382b41194ac6c
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-04-01 19:04:48 +0000

    chore: Add GrammaticalGenderDetector class

---
+++ src/GenderDetection/GrammaticalGenderDetector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shevchenko\GenderDetection;
+
+class GrammaticalGenderDetector
+{
+    public function __construct(
+        private readonly string $masculinePattern,
+        private readonly string $femininePattern,
+    ) {}
+
+    public function detect(string $name): ?string
+    {
+        if (preg_match('/(' . $this->masculinePattern . ')$/iu', $name)) {
+            return 'masculine';
+        }
+
+        if (preg_match('/(' . $this->femininePattern . ')$/iu', $name)) {
+            return 'feminine';
+        }
+
+        return null;
+    }
+}
